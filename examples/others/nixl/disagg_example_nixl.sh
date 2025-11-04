89737935289b516121cc3cf053b90ae76abc8490
Sundaresan-G <sundaresan.g@intel.com>
2025-11-03 22:30:13 -0800

    An example for disagg PD with GPU-CPU added

---
+++ examples/others/nixl/disagg_example_nixl.sh
@@ -0,0 +1,206 @@
+#!/bin/bash
+
+# echo "Warning: LMCache disaggregated prefill support for vLLM v1 is experimental and subject to change."
+
+set -xe
+
+PIDS=()
+MODEL="Qwen/Qwen2.5-1.5B-Instruct"
+INPUT_LEN=1024
+OUTPUT_LEN=8
+NUM_PROMPTS=1
+
+# Switch to the directory of the current script
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+check_hf_token() {
+    if [ -z "$HF_TOKEN" ]; then
+        echo "HF_TOKEN is not set. Please set it to your Hugging Face token."
+        exit 1
+    fi
+    if [[ "$HF_TOKEN" != hf_* ]]; then
+        echo "HF_TOKEN is not a valid Hugging Face token. Please set it to your Hugging Face token."
+        exit 1
+    fi
+    echo "HF_TOKEN is set and valid."
+}
+
+check_num_gpus() {
+    # can you check if the number of GPUs are >=2 via nvidia-smi/rocm-smi?
+    which rocm-smi > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+	num_gpus=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+    else
+	num_gpus=$(rocm-smi --showid | grep Instinct | wc -l)
+    fi
+
+    if [ "$num_gpus" -lt 2 ]; then
+        echo "You need at least 2 GPUs to run disaggregated prefill."
+        exit 1
+    else
+        echo "Found $num_gpus GPUs."
+    fi
+}
+
+ensure_python_library_installed() {
+    echo "Checking if $1 is installed..."
+    python3 -c "import $1" > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        if [ "$1" == "nixl" ]; then
+            echo "$1 is not installed. Please refer to https://github.com/ai-dynamo/nixl for installation."
+        else
+            echo "$1 is not installed. Please install it via pip install $1."
+        fi
+        exit 1
+    else
+        echo "$1 is installed."
+    fi
+}
+
+cleanup() {
+    echo "Stopping everything…"
+    trap - INT TERM        # prevent re-entrancy
+    kill -9 -- -$$            # negative PID  ==  “this whole process-group”
+    wait                   # reap children so we don't leave zombies
+    exit 0
+}
+
+wait_for_server() {
+  local port=$1
+  local timeout_seconds=1200
+  local start_time=$(date +%s)
+
+  echo "Waiting for server on port $port..."
+
+  while true; do
+    if curl -s "localhost:${port}/healthcheck" > /dev/null; then
+      return 0
+    # Fallback to POST request to completions
+    elif curl -s -X POST "localhost:${port}/v1/completions" \
+        -H "Content-Type: application/json" \
+        -d '{"model": "test", "prompt": "test", "max_tokens": 1}' > /dev/null 2>&1; then
+      return 0
+    fi
+
+    local now=$(date +%s)
+    echo "Waiting for server on port $port..."
+    if (( now - start_time >= timeout_seconds )); then
+      echo "Timeout waiting for server"
+      return 1
+    fi
+
+    sleep 1
+  done
+}
+
+
+main() {
+
+    source /data/nfs_home/sundares/miniforge3/etc/profile.d/conda.sh
+    # conda activate vllm_cpu
+    conda activate vllm_gpu
+
+    # check_hf_token
+    # check_num_gpus
+    # ensure_python_library_installed lmcache
+    # ensure_python_library_installed nixl
+    # ensure_python_library_installed pandas
+    # ensure_python_library_installed datasets
+    # ensure_python_library_installed vllm
+
+    trap cleanup INT
+    trap cleanup USR1
+    trap cleanup TERM
+
+    echo "Launching prefiller, decoder and proxy..."
+    echo "Please check prefiller.log, decoder.log and proxy.log for logs."
+    export NIXL_DEBUG_LOGGING=1
+
+    KV_BUFFER_DEVICE="cuda" bash prefiller_decoder_vllm_launcher.sh prefiller $MODEL \
+        > >(tee prefiller.log) 2>&1 &
+    prefiller_pid=$!
+    PIDS+=($prefiller_pid)
+
+    conda activate vllm_cpu
+
+    KV_BUFFER_DEVICE="cpu" bash prefiller_decoder_vllm_launcher.sh decoder $MODEL \
+        > >(tee decoder.log)  2>&1 &
+    decoder_pid=$!
+    PIDS+=($decoder_pid)
+
+    conda activate vllm_gpu
+
+    # Use proxy_server.py or toy_proxy_server.py
+    # python -m debugpy --listen 0.0.0.0:5678 --wait-for-client \
+    python toy_proxy_server.py \
+        --host localhost \
+        --port 9000 \
+        --prefiller-host localhost \
+        --prefiller-port 8100 \
+        --decoder-host localhost \
+        --decoder-port 8200  \
+        > >(tee proxy.log)    2>&1 &
+
+    proxy_pid=$!
+    PIDS+=($proxy_pid)
+
+    wait_for_server 8100
+    wait_for_server 8200
+    wait_for_server 9000
+
+    # echo "All servers are up. Starting benchmark..."
+
+    # begin benchmark
+    # cd ../../../benchmarks/
+
+    # python -m debugpy --listen 0.0.0.0:5678 --wait-for-client \
+    $(which vllm) bench serve --port 9000 --seed $(date +%s) \
+        --model $MODEL \
+        --dataset-name random --random-input-len $INPUT_LEN --random-output-len $OUTPUT_LEN \
+        --num-prompts $NUM_PROMPTS \
+        2>&1 | tee benchmark.log
+
+    # while true; do
+    #     # python -m debugpy --listen 0.0.0.0:5678 --wait-for-client \
+    #     $(which vllm) bench serve --port 9000 --seed $(date +%s) \
+    #         --model $MODEL \
+    #         --dataset-name random --random-input-len $INPUT_LEN --random-output-len $OUTPUT_LEN \
+    #         --num-prompts $NUM_PROMPTS \
+    #         2>&1 | tee benchmark.log
+    # done
+
+    # vllm bench serve --port 9000 --seed $(date +%s) \
+    #     --model $MODEL \
+    #     --dataset-name random --random-input-len 20 --random-output-len 10 \
+    #     --num-prompts 2 --burstiness 1 --request-rate 3.6 | tee benchmark.log
+
+    # vllm bench latency --port 9000 \
+    #     --model $MODEL \
+
+    # echo "Benchmarking done. Cleaning up..."
+
+    # cleanup
+
+    echo "==================================================="
+    echo "All servers are up. You can send request now..."
+    echo "Press Ctrl-C to terminate all instances."
+
+    # Keep the script running until interrupted
+    echo "Script is running. Waiting for termination signal..."
+    echo "==================================================="
+
+    # vllm bench serve --port 9000 --seed $(date +%s) \
+    #     --model $MODEL \
+    #     --dataset-name random --random-input-len $INPUT_LEN --random-output-len $OUTPUT_LEN \
+    #     --num-prompts $NUM_PROMPTS \
+    #     2>&1 | tee benchmark.log
+
+    # while true; do
+    #     sleep 1
+    # done
+
+    cleanup
+
+}
+
+main
---
+++ examples/others/nixl/prefiller_decoder_vllm_launcher.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+set -x
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if [[ $# -lt 1 ]]; then
+    echo "Usage: $0 <prefiller | decoder> [model]"
+    exit 1
+fi
+
+if [[ $# -eq 1 ]]; then
+    echo "Using default model: meta-llama/Meta-Llama-3.1-8B"
+    MODEL="meta-llama/Meta-Llama-3.1-8B"
+else
+    echo "Using model: $2"
+    MODEL=$2
+fi
+
+# The prefillers and decoders in LMCache use the same hash seed for all chunk keys.
+# This seed must be aligned so that decoders can identify and retrieve KV cache
+# entries stored by prefillers.
+#
+# WARNING: Using a fixed hash seed is insecure and makes the application vulnerable to
+# denial-of-service attacks. In a production environment, this should be set to a
+# secure random value. This is set to a fixed value for demonstration purposes only.
+export PYTHONHASHSEED=${VLLM_PYTHON_HASH_SEED:-123}
+export VLLM_LOGGING_LEVEL=DEBUG
+export TORCH_CUDA_ARCH_LIST="12.0"
+export UCX_NET_DEVICES=all
+export UCX_TLS=all
+export VLLM_TORCH_PROFILER_WITH_STACK=1 
+export VLLM_TORCH_PROFILER_WITH_FLOPS=1
+export VLLM_TORCH_PROFILER_RECORD_SHAPES=1
+export VLLM_TORCH_PROFILER_DIR="."
+export BLOCK_SIZE=32
+
+if [[ $1 == "prefiller" ]]; then
+
+    # python -m debugpy --listen 0.0.0.0:5678 --wait-for-client \
+
+    # 1st GPU as prefiller
+    # OMP_NUM_THREADS=16 \
+    VLLM_CPU_OMP_THREADS_BIND="0-31" \
+    VLLM_CPU_SGL_KERNEL="1" \
+    VLLM_NIXL_SIDE_CHANNEL_PORT=5600 \
+    VLLM_DOUBLE_BUFFER_PIPELINE=1 \
+    CUDA_VISIBLE_DEVICES=0 \
+    numactl -C 0-31 \
+    $(which vllm) serve $MODEL \
+    --port 8100 \
+    --max-model-len 2600 \
+    --max-num-seqs 512 \
+    --max-num-batched-tokens 4096 \
+    --block-size $BLOCK_SIZE \
+    --kv-transfer-config '{"kv_connector":"NixlConnector","kv_role":"kv_both","kv_buffer_device":"'"$KV_BUFFER_DEVICE"'"}' \
+    --enforce-eager
+    
+elif [[ $1 == "decoder" ]]; then
+    # Decoder listens on port 8200
+
+    source /swtools/cuda/12.9.0/cuda_vars.sh
+
+    # 2nd GPU as decoder
+    # OMP_NUM_THREADS=32 \
+    VLLM_CPU_OMP_THREADS_BIND="32-63" \
+    VLLM_CPU_SGL_KERNEL="1" \
+    VLLM_NIXL_SIDE_CHANNEL_PORT=5601 \
+    VLLM_DOUBLE_BUFFER_PIPELINE=0 \
+    CUDA_VISIBLE_DEVICES=1 \
+    numactl -C 32-63 \
+    $(which vllm) serve $MODEL \
+    --port 8200 \
+    --enforce-eager \
+    --max-model-len 2600 \
+    --max-num-seqs 512 \
+    --max-num-batched-tokens 4096 \
+    --block-size $BLOCK_SIZE \
+    --kv-transfer-config '{"kv_connector":"NixlConnector","kv_role":"kv_both","kv_buffer_device":"'"$KV_BUFFER_DEVICE"'"}'
+
+else
+    echo "Invalid role: $1"
+    echo "Should be either prefiller, decoder"
+    exit 1
+fi
